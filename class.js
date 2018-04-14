9ac598b8c9459b367b168739f55383f5b817f70b
BENSAADI Nour Elhouda <37048780+NourEngineer@users.noreply.github.com>
2018-04-14 16:59:49 +0200

    Update class.js

--- class.js
+++ class.js
@@ -126,7 +126,7 @@ function Contact(nom,prenom,adresse,email,numero,siteweb){
 }
 
 function affichercontact(clas,T,n,l){
-		var div = $("<div></div>").attr("id","c"+l+n).css({"width":"100%","height":"70px","position":"relative","z-index":"1"});//.width(100);"margin-top":"9px""display":"inline-block",
+		var div = $("<div></div>").attr("id","c"+l+n).css({"width":"50%","height":"70px","position":"relative","z-index":"1"});//.width(100);"margin-top":"9px""display":"inline-block",
 		$("#"+clas).append(div);
 		var image = $("<div></div>").attr("id","image"+l+n).css({"width":"15%","float":"left","height":"70px","padding-top":"10px"});//.width(20%);
 		$("#c"+l+n).append(image);
